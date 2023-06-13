4840b84fe422db74d6989a3c29b132a740c51490
MICHAELKITH <michaelkithinji27@gmail.com>
2023-06-13 16:43:15 +0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  root to: 'users#index'
+  resources :users do
+    resources :posts
   end
 end
